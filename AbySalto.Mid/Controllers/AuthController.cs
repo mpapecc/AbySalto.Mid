7d7cd703b1299415ef5cc291c66a4305d9a38c6e
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-06-24 20:03:48 +0200

    Auth controller

---
+++ AbySalto.Mid/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using AbySalto.Mid.Application.Interfaces.Auth;
+using AbySalto.Mid.Application.Models.Auth;
+using AbySalto.Mid.Infrastructure.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbySalto.Mid.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService authService;
+        private readonly IUserIdentity userIdentity;
+
+        public AuthController(IAuthService authService, IUserIdentity userIdentity)
+        {
+            this.authService = authService;
+            this.userIdentity = userIdentity;
+        }
+
+        [HttpPost(nameof(Login))]
+        public AuthResponse Login(Login login)
+        {
+           return this.authService.Login(login);
+        }
+
+        [HttpPost(nameof(Register))]
+        public AuthResponse Register(Registration registration)
+        {
+            return this.authService.Register(registration);
+        }
+
+        [Authorize]
+        [HttpGet(nameof(WhoAmI))]
+        public IUserIdentity WhoAmI()
+        {
+            return this.userIdentity;
+        }
+    }
+}
