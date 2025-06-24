0130427e3a1cebc8c5f9d68cd25cd1d6da5390cd
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-06-24 19:51:55 +0200

    add Id model binder and json converter

---
+++ AbySalto.Mid.Application/JsonConverters/IdConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using AbySalto.Mid.Application.Models;
+
+namespace AbySalto.Mid.Application.JsonConverters
+{
+    public class IdConverter : JsonConverter<Id>
+    {
+        public override Id Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var rawId = reader.GetInt32();
+
+            return new Id(rawId);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Id value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.EncryptedId);
+        }
+    }
+}
---
+++ AbySalto.Mid.Application/ModelBinders/IdModelBinder.cs
@@ -0,0 +1,51 @@
+﻿using AbySalto.Mid.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AbySalto.Mid.Application.ModelBinders
+{
+    public class IdModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(nameof(Id));
+
+            if (valueProviderResult.Length == 0)
+                valueProviderResult = bindingContext.ValueProvider.GetValue(nameof(Id.EncryptedId));
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            string encryptedId = valueProviderResult.FirstValue;
+
+            if (string.IsNullOrEmpty(encryptedId))
+            {
+                return Task.CompletedTask;
+            }
+
+            try
+            {
+                var result = new Id(Id.Decrypt(encryptedId));
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+            catch
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Invalid Id.");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class IdAttribute : ModelBinderAttribute
+    {
+        public IdAttribute() : base(typeof(IdModelBinder))
+        {
+            BindingSource = BindingSource.Query;
+            Name = "id";
+        }
+    }
+
+}
